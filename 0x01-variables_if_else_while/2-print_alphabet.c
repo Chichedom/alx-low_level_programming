9d001708d8ac6f442faf1b08a38c8b50e0b908c2
Chichedom <chichedomaaron@gmail.com>
2022-09-13 04:46:47 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main(void)
-{
-int letter;
-for (letter = 97; letter <= 122; letter++)
+{char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-putchar("\n");
+putchar('\n');
 return (0);
 }

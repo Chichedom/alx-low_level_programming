a993fddcdb3c0f1b9e872f1a0ad978b604dc48e5
Chichedom <chichedomaaron@gmail.com>
2022-09-13 21:35:59 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -9,6 +10,6 @@ void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)   
-_putchar(letter);
-_putchar('\n');
+putchar(letter);
+putchar('\n');
 }

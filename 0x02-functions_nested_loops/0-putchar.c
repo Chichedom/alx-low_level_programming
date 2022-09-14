59f15cb558a950a857729177e923ebe27ceee521
Chichedom <chichedomaaron@gmail.com>
2022-09-13 21:26:24 -0700

    trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,17 @@
-/**
- * File: 0-putchar.c
- * Auth: Chichedom
- */
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - This function prints "putchar"
+ * main - this function prints "_putchar"
  * Return: Always 0.
  */
 
 
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
+char *p  = "_putchar";
+while (*p)
+putchar (*p);
+*p++;
 putchar('\n');
 return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+
+#define MAIN_H_
+
+
+void print_alphabet(void);
+int main(void) ;
+#endif
